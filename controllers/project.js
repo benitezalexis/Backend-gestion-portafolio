ee9f5adc66e438bd698b7a83431fdfff5b9549bc
benitezalexis <abenitezcolman96@gmail.com>
2021-09-15 21:22:14 -0300

    CRUD de proyectos

--- controllers/project.js
+++ controllers/project.js
@@ -2,6 +2,7 @@
 
 
 var Project=require('../models/project');
+
 var controller= {
 	home: function(req, res){
 		return res.status(200).send({
@@ -33,11 +34,65 @@ var controller= {
 
 			return res.status(200).send({project: projectStored});
 		});
+	},
 
+	getProject: function(req,res){
+		var projectId=req.params.id;
 
+		if (projectId==null) return res.status(404).send({message: 'El documento o proyecto no existe'}); 
 		
-	}
 
+		Project.findById(projectId,(err, projects)=>{
+			if(err)return res.status(500).send({message: 'Error al devolver los datos'});
+
+			if(!projects) return res.status(404).send({message: 'El documento o proyecto no existe'});
+
+			return res.status(200).send({projects});
+		});
+
+	},
+
+	getProjects: function(req,res){
+		Project.find({}).sort('-year').exec((err, projectss)=>{
+			if(err) return res.status(500).send({message: 'Error al devolver los datos'});
+
+			if(!projectss) return res.status(404).send({message: 'No hay proyectos para mostrar'});
+
+			return res.status(200).send({projectss});
+		});
+	},
+	updateProject: function(req,res){
+		var projectId=req.params.id;
+		var update=req.body;
+
+		Project.findByIdAndUpdate(projectId, update, {new:true}, (err,projectUpdated)=>{
+			if(err) return res.status(500).send({message: 'Error al actualizar'});
+
+			if(!projectUpdated) return res.status(404).send({message: 'No existe le proyecto a actualizar'});
+
+			return res.status(200).send({
+				project: projectUpdated
+			})
+
+		} );
+
+
+	},
+
+	deleteProject: function(req,res){
+		var projectId=req.params.id;
+
+		Project.findByIdAndRemove(projectId, (err,projectDelete)=>{
+			if(err)return res.status(500).send({message: 'No se ha popido borrar el documento'});
+
+			if(!projectDelete) return res.status(404).send({message: 'No se puede eliminar ese proyecto'});
+
+			return res.status(200).send({
+				project: projectDelete
+			});
+		});
+
+	}
 
 };
 
